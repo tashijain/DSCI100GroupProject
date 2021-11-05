101d9cdbe9d31954ba13cad7c4ca4b6f76d5cff6
Mapleia <36678302+Mapleia@users.noreply.github.com>
2021-11-04 18:28:32 -0700

    specified predictor cols

--- main.R
+++ main.R
@@ -1,8 +1,8 @@
 # Define Data Frame Loader Function # ----
 library(tidyverse)
 load_main_df <- function() {
-  colnames <- c("age", "sex", "cp", "testbps", "chol", "fbs", "restecg", "thalach",
-                "exang", "oldpeak", "slope", "ca", "thal", "predicted")
+  colnames <- c("age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach",
+                "exang", "oldpeak", "slope", "ca", "thal", "presense")
   
   heart_disease_data <- read_delim("data/reprocessed.hungarian.data", 
                                delim = " ",
@@ -29,7 +29,8 @@ load_main_df <- function() {
     # increment to keep track of the position in the list of files
     i<-i+1
   }
-  return(heart_disease_data)
+  return(select(heart_disease_data, age, trestbps, chol, thalach, oldpeak, 
+                region, presense))
 }
 
 # Load Main Data Frame # ----
@@ -38,14 +39,24 @@ heart_disease_data <- load_main_df()
 # See Summary of All Columns # ----
 summary(heart_disease_data)
 
+# Columns that can be selected #----
+# 5 variables, choose 3 => 10 variations
+# "presense" <- col to predict
+picked_cols <- c("age", "trestbps", "chol", "thalach", "oldpeak")
+# age: age in years 
+# trestbps: resting blood pressure (in mm Hg on admission to the hospital) 
+# chol: serum cholesterol in mg/dl 
+# thalach: maximum heart rate achieved
+# oldpeak: ST depression induced by exercise relative to rest 
+
 # Make Histogram plot # ----
 library(ggplot2)
 ggplot(heart_disease_data, aes(x = chol)) + 
   geom_histogram() +
-  facet_grid(rows = vars(region))
+  facet_grid(rows = vars(region), cols = vars(presense))
 
 # Make a Whisker Box Plot # ----
 ggplot(heart_disease_data, aes(x = region)) + 
   geom_boxplot() + 
-  facet_grid(rows = vars(cp, chol, restecg))
+  facet_grid(rows = vars(trestbps, chol, thalach, oldpeak))
 
